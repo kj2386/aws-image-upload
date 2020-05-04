3e8023cf93b1b7490e4cd7bfec0d5f6f049bcf03
kj2386 <kj2386@gmail.com>
2020-05-04 13:57:36 -0600

    uploadUserProfileImage code added

--- src/main/java/tech/alexjohnson/awsimageupload/profile/UserProfileController.java
+++ src/main/java/tech/alexjohnson/awsimageupload/profile/UserProfileController.java
@@ -5,12 +5,13 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.awt.*;
+
 import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/user-profile")
+@CrossOrigin("*")
 public class UserProfileController {
 
     private final UserProfileService userProfileService;
@@ -26,7 +27,7 @@ public class UserProfileController {
     }
 
     @PostMapping(
-            path = "{userProfileId}/image/download",
+            path = "{userProfileId}/image/upload",
             consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
--- src/main/java/tech/alexjohnson/awsimageupload/profile/UserProfileService.java
+++ src/main/java/tech/alexjohnson/awsimageupload/profile/UserProfileService.java
@@ -1,27 +1,60 @@
 package tech.alexjohnson.awsimageupload.profile;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import tech.alexjohnson.awsimageupload.bucket.BucketName;
+import tech.alexjohnson.awsimageupload.filestore.FileStore;
 
-import java.util.List;
-import java.util.UUID;
+import java.io.IOException;
+import java.util.*;
+
+import static org.apache.http.entity.ContentType.*;
 
 @Service
 public class UserProfileService {
 
     private final UserProfileDataAccessService userProfileDataAccessService;
+    private final FileStore fileStore;
 
     @Autowired
-    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService) {
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService, FileStore fileStore) {
         this.userProfileDataAccessService = userProfileDataAccessService;
+        this.fileStore = fileStore;
     }
 
     List<UserProfile> getUserProfiles() {
         return userProfileDataAccessService.getUserProfiles();
     }
 
-    public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+    void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+        if (file.isEmpty()) {
+            throw new IllegalStateException("Cannot upload empty file [" + file.getSize() + "]");
+        }
+        if (!Arrays.asList(IMAGE_JPEG, IMAGE_PNG, IMAGE_GIF).contains(file.getContentType())) {
+            throw new IllegalStateException("File must be an image");
+        }
+        
+        UserProfile user = userProfileDataAccessService
+                .getUserProfiles()
+                .stream()
+                .filter(userProfile -> userProfile.getUserProfileId().equals(userProfileId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException(String.format("User profile %s not found", userProfileId)));
 
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("Content-Type", file.getContentType());
+        metadata.put("Content-Length", String.valueOf(file.getSize()));
+
+        String path = String.format("%s/%s", BucketName.PROFILE_IMAGE.getBucketName(), user.getUserProfileId());
+        String filename = String.format("%s-%s", file.getName(), UUID.randomUUID());
+
+        try {
+            fileStore.save(path, filename, Optional.of(metadata), file.getInputStream());
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
+
